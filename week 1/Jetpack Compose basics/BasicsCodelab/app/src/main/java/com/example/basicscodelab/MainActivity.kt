2e294ae47415a7bdf4f9333cc22292e3384746fd
dong3789 <dhovey@gmail.com>
2021-11-05 00:31:32 +0900

    [2021-11-05] gradle 오류 수정, show 버튼 -> 아이콘 변경

--- week 1/Jetpack Compose basics/BasicsCodelab/app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ week 1/Jetpack Compose basics/BasicsCodelab/app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.example.basicscodelab
 
 import android.annotation.SuppressLint
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basicscodelab.ui.theme.BasicsCodelabTheme
@@ -33,35 +47,15 @@ class MainActivity : ComponentActivity() {
 //@Preview(showBackground = true, withDp = 300)
 @Composable
 fun Greeting(name: String) {
-    val expanded = remember{ mutableStateOf(false) }
-    val extraPadding = if( expanded.value ) 50.dp else 0.dp
-    androidx.compose.material.Surface(color = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 5.dp)
-        ) {
-        Row(modifier = Modifier.padding(24.dp)){
-            Column(modifier = Modifier
-                .weight(1f)
-                .padding(bottom = extraPadding)
-                ) {
-                Text(text = "Hello")
-                Text(text = "$name!!!")
-            }
-            OutlinedButton(
-                onClick = { expanded.value = !expanded.value }
-            ) {
-                Text( if(expanded.value) "Show less" else "Show MORE")
-            }
-        }
-
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ){
+        CardContent(name)
     }
-}
+    // animateDpAsState는 애니메이션 api
+    var expanded by remember{ mutableStateOf(false) }
 
-@Preview(showBackground = true, name = "Text preview")
-@Composable
-fun DefaultPreview() {
-    BasicsCodelabTheme {
-        Greetings()
-    }
 }
 
 //@Preview(showBackground = true, widthDp = 300)
@@ -70,7 +64,12 @@ private fun MyApp() {
 //    androidx.compose.material.Surface(color = MaterialTheme.colors.background) {
 //        Greeting("Let's Study Compose!~")
 //    }
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    //# 지속 상태
+    //var shouldShowOnboarding by remember{ mutableStateOf(true) }
+
+    //# remember는 컴포저블이 컴포지션에 유지되는 동안에만 동작
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
     if( shouldShowOnboarding ){
         OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
     }else{
@@ -87,11 +86,6 @@ private fun Greetings(names: List<String> = List(100) { "$it" } ) {
             Greeting(name = name)
         }
     }
-//    Column(modifier = Modifier.padding(vertical = 4.dp)) {
-//        for(name in names){
-//            Greeting(name = name)
-//        }
-//    }
 }
 
 @Composable
@@ -112,6 +106,65 @@ fun OnboardingScreen(onContinueClicked: () -> Unit) {
             }
 
         }
+    }
+}
+
+
+@Composable
+private fun CardContent(name: String) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello, ")
+            Text(
+                text = name,
+                style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            if (expanded) {
+                Text(
+                    text = ("compose 텍스트 테스트 " +
+                            "compose 텍스트 테스트 중입니다").repeat(4),
+                )
+            }
+        }
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if (expanded) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                }
+
+            )
+        }
+    }
+}
+
+
+@Preview(showBackground = true, widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES, name = "DefaultPreviewDark")
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun DefaultPreview() {
+    BasicsCodelabTheme {
+        Greetings()
     }
 }
 
