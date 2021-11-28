3688d77f5c16f73dd1144fc5a660ef1ef1e2a216
dong3789 <dhovey@gmail.com>
2021-11-28 19:54:46 +0900

    week 4-1 upload

---
+++ week 4/Testing in Jetpack Compose/app/src/main/java/com/example/compose/rally/OverviewScreenTest.kt
@@ -0,0 +1,17 @@
+package com.example.compose.rally
+
+class OverviewScreenTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun overviewScreen_alertsDisplayed() {
+        composeTestRule.setContent {
+            OverviewBody()
+        }
+
+        composeTestRule
+            .onNodeWithText("Alerts")
+            .assertIsDisplayed()
+    }
+}
